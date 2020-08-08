56f816903288a96d83a9263b4986bbce89ddb660
MuhammadSaadSheikh <msaad9124@gmail.com>
2020-08-08 11:02:52 -0700

    practice-01

--- myapp/App.js
+++ myapp/App.js
@@ -8,45 +8,7 @@ import {styler} from './style'
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ImageBackground source={require('./assets/x-men.jpg')} style={styler.img}> */}
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-        {/* <TouchableOpacity onPress={()=>alert('hello')} style={styler.btn}>
-          <Text>New Button</Text>
-        </TouchableOpacity> */}
-        {/* <Button onPress={() => alert('hey!')} title='Say hi' colr='green'></Button> */}
-        {/* <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight> */}
-        <View style={styler.scrlWrapper}>
-          <ScrollView contentContainerStyle={styler.scrl}>
-        <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-                <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-                <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-                <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-                <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-                <TouchableHighlight onPress={()=>alert('hello yar')} style={styler.hightLight}>
-          <Text style={styler.txtSize}>Work</Text>
-        </TouchableHighlight>
-          </ScrollView>
-      </View>
-        <TouchableOpacity onPress={() => alert("avengers")}>
-          <Image
-            source={require("./assets/x-men.jpg")}
-            style={styler.img}
-          />
-        </TouchableOpacity>
-      <ActivityIndicator />
-      {/* </ImageBackground> */}
+      <Text>Helo World</Text>
     </View>
   );
 }
